2c7d7b39767ff411df178e691848a33bcba1ae9a
sonychinnala <sonychinnala@gmail.com>
2024-09-28 21:04:49 +0530

    Create Views5.py

---
+++ Views5.py
@@ -0,0 +1,13 @@
+def add_blogs(request):
+    if request.method=="POST":
+        form = BlogPostForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost.save()
+            obj = form.instance
+            alert = True
+            return render(request, "add_blogs.html",{'obj':obj, 'alert':alert})
+    else:
+        form=BlogPostForm()
+    return render(request, "add_blogs.html", {'form':form})
