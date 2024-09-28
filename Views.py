f177c6598a0f6f35fd2f9f5fd549821b60012d89
sonychinnala <sonychinnala@gmail.com>
2024-09-28 20:54:23 +0530

    Create Views.py

---
+++ Views.py
@@ -0,0 +1,19 @@
+def Register(request):
+    If request.method=="POST":   
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 != password2:
+            messages.error(request, "Passwords do not match.")
+            return redirect('/register')
+ 
+        user = User.objects.create_user(username, email, password1)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return render(request, 'login.html')  
+    return render(request, "register.html")
