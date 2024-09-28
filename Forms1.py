792215f89fa35c518ec87e77eaf4f6c73a88510c
sonychinnala <sonychinnala@gmail.com>
2024-09-28 21:07:43 +0530

    Create Forms1.py

---
+++ Forms1.py
@@ -0,0 +1,9 @@
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'slug', 'content', 'image')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title of the Blog'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+        }
