a8c3f02aa135810500f3ac9f65a6c28dfa224d96
sonychinnala <sonychinnala@gmail.com>
2024-09-28 20:57:49 +0530

    Create Views1.py

---
+++ Views1.py
@@ -0,0 +1,15 @@
+def Login(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid Credentials")
+        return render(request, 'blog.html')   
+    return render(request, "login.html")
