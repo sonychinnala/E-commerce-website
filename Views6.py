da64c87b2bce18019812128821158f1158b090ba
sonychinnala <sonychinnala@gmail.com>
2024-09-28 21:09:16 +0530

    Create Views6.py

---
+++ Views6.py
@@ -0,0 +1,10 @@
+def blogs_comments(request, slug):
+    post = BlogPost.objects.filter(slug=slug).first()
+    comments = Comment.objects.filter(blog=post)
+    if request.method=="POST":
+        user = request.user
+        content = request.POST.get('content','')
+        blog_id =request.POST.get('blog_id','')
+        comment = Comment(user = user, content = content, blog=post)
+        comment.save()
+    return render(request, "blog_comments.html", {'post':post, 'comments':comments}) 
