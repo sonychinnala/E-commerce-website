066c872ad5d6ac9fb1fd922cc706ae953d408388
sonychinnala <sonychinnala@gmail.com>
2024-09-28 21:00:42 +0530

    Create Views3.py

---
+++ Views3.py
@@ -0,0 +1,14 @@
+def edit_profile(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method=="POST":
+        form = ProfileForm(data=request.POST, files=request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            alert = True
+            return render(request, "edit_profile.html", {'alert':alert})
+    else:
+        form=ProfileForm(instance=profile)
+    return render(request, "edit_profile.html", {'form':form})
